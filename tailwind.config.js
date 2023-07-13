dba53172460dbf9e903114594bb0868a21f5ba61
moloruntomiwa31 <moloruntomiwa31@gmail.com>
2023-07-13 15:10:08 +0100

    initial files

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  screens: {
+    sm: "480px",
+    md: "668px",
+    lg: "960px",
+    xl: "1440px"
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
